67bbd5673e2529c4aa9dfabdd6c46bf4a66ac50e
Luna712 <142361265+Luna712@users.noreply.github.com>
2023-10-24 02:01:22 -0600

    Fix

--- SoraStream/src/main/kotlin/com/hexated/SoraStream.kt
+++ SoraStream/src/main/kotlin/com/hexated/SoraStream.kt
@@ -320,7 +320,7 @@ open class SoraStream : TmdbProvider() {
                 this.showStatus = getStatus(res.status)
                 this.recommendations = recommendations
                 this.actors = actors
-                this.contentRating = fetchContentRating(id, "US", true)
+                this.contentRating = fetchContentRating(data.id, "US", true)
                 addTrailer(trailer)
                 addTMDbId(data.id.toString())
                 addImdbId(res.external_ids?.imdb_id)
@@ -355,7 +355,7 @@ open class SoraStream : TmdbProvider() {
                 this.rating = rating
                 this.recommendations = recommendations
                 this.actors = actors
-                this.contentRating = fetchContentRating(id, "US", false)
+                this.contentRating = fetchContentRating(data.id, "US", false)
                 addTrailer(trailer)
                 addTMDbId(data.id.toString())
                 addImdbId(res.external_ids?.imdb_id)
